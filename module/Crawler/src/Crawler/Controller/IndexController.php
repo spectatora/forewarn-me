5e797f411274e301130849a8f32fe2ae121c0e51
Lyubomir Filipov <lyubomir@acewebdesign.com.au>
2016-05-02 23:23:17 +0300

    proof of concept when reading data from vik vt

--- module/Crawler/src/Crawler/Controller/IndexController.php
+++ module/Crawler/src/Crawler/Controller/IndexController.php
@@ -3,6 +3,7 @@ namespace Crawler\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Symfony\Component\DomCrawler\Crawler;
 
 class IndexController extends AbstractActionController
 {
@@ -10,7 +11,34 @@ class IndexController extends AbstractActionController
     {
 
 
-        print 'inside crawler action';
+        $newUrl = "http://www.vik-vt.com/?mod=news";
+        $crawler = new Crawler;
+        $crawler->addHTMLContent(file_get_contents($newUrl), 'UTF-8');
+
+        if ($crawler->filter("body")->text() == "404")
+        {
+            return array();
+        }
+
+        $firstDate = $crawler->filterXPath("//div[@class='mb5 date']")->first()->text();
+        $firstIdentifier = $crawler->filterXPath("//div[@class='mb15 text_06']/a")->first()->attr("href");
+
+
+        $nodeValues = $crawler->filter('div.list_item')->each(function ($node, $i) {
+
+            $time = $node->filterXPath("//div[@class='mb5 date']")->text();
+            $identifier = $node->filterXPath("//div[@class='mb15 text_06']/a")->first()->attr("href");
+
+            return array(
+                'time' => $time,
+                'identifier' => $identifier
+            );
+
+        });
+
+
+        var_dump($nodeValues);
+
 
         return $this->getResponse();
     }
